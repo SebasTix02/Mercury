d392a0db876c1ada3ab34faa50f65270a7908b73
Andrés <andrestacuaman13@gmail.com>
2024-06-04 08:25:42 -0500

    Creacion del Inventario Bienes Generales

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -25,11 +25,8 @@ import { Brands } from "./pages/brand/brand";
 import { Dependency } from "./pages/dependency/dependency";
 import { Software } from "./pages/inventory/software";
 import { Inventario_Computadores } from "./pages/inventory/computers";
-<<<<<<< HEAD
-=======
-import Reports from "./pages/reports/reports";
+//import Reports from "./pages/reports/reports";
 import { Inventario_Bienes } from "./pages/inventory/bienes";
->>>>>>> a68dc7c (Creacion del Inventario Bienes Generales)
 
 
 function App() {
@@ -69,11 +66,8 @@ function App() {
                   <Route path="/dependencias" element={<Dependency />} />
                   <Route path="/software" element={<Software />} />
                   <Route path="/computadores" element={<Inventario_Computadores />} />
-<<<<<<< HEAD
-=======
-                  <Route path="/reportes" element={<Reports />} />
                   <Route path="/bienes" element={<Inventario_Bienes/>}/>
->>>>>>> a68dc7c (Creacion del Inventario Bienes Generales)
+
 
                 </Routes>
                 <RefineKbar />
