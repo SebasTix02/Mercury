4a81c8eacb4fd4eb65198da91dfc4ad1177e10bd
Juanes <jespin2876@uta.edu.ec>
2024-06-11 19:22:24 -0500

    Se ha agregado el campo de rol en la tabla users. Se ha modificado los servicios de lectura, escritura y actualización, así como la documentación API. This fixed #65

--- backend/controllers/login.js
+++ backend/controllers/login.js
@@ -4,7 +4,13 @@ exports.validateCredentials = async (request, response) => {
     try{
         const {username, password} = request.body;
         const [data] = await connection.query(
-            'SELECT ID_NUMBER, FIRST_NAME, LASTNAME FROM USER WHERE EMAIL = ? AND PASSWORD = ?',
+            `SELECT ID_NUMBER, FIRST_NAME, LASTNAME, 
+            CASE 
+                WHEN ROLE = 0 THEN 'USUARIO'
+                WHEN ROLE = 1 THEN 'LABORATORISTA'
+                WHEN ROLE = 2 THEN 'ADMIN'
+            END AS ROLE
+            FROM USER WHERE EMAIL = ? AND PASSWORD = ?`,
             [username,password]);
         if(data.length == 0){
             response.status(500).json({error: 'Credenciales Incorrectas'});
